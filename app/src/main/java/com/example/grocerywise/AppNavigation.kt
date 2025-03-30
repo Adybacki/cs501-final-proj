f0aa3aefa4ba38b370f857cd982f886b6af047d4
Adybacki <adybacki04@gmail.com>
2025-03-30 18:17:06 -0400

    added Firestore authentication for user login and signup

---
+++ app/src/main/java/com/example/grocerywise/AppNavigation.kt
@@ -0,0 +1,27 @@
+package com.example.grocerywise
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.grocerywise.pages.HomePage
+import com.example.grocerywise.pages.LoginPage
+import com.example.grocerywise.pages.SignupPage
+
+@Composable
+fun AppNavigation(modifier: Modifier = Modifier, authViewModel: AuthViewModel) {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "login", builder = {
+        composable("login"){
+            LoginPage(modifier, navController, authViewModel)
+        }
+        composable("signup"){
+            SignupPage(modifier, navController, authViewModel)
+        }
+        composable("home"){
+            HomePage(modifier, navController, authViewModel)
+        }
+    })
+}
---
+++ app/src/main/java/com/example/grocerywise/pages/HomePage.kt
@@ -0,0 +1,39 @@
+package com.example.grocerywise.pages
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.grocerywise.AuthState
+import com.example.grocerywise.AuthViewModel
+
+@Composable
+fun HomePage(modifier: Modifier = Modifier, navController: NavController, authViewModel: AuthViewModel) {
+    val authState = authViewModel.authState.observeAsState()
+
+    LaunchedEffect(authState.value) {
+        when (authState.value) {
+            is AuthState.Unauthenticated -> navController.navigate("login")
+            else -> Unit
+        }
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TextButton(onClick = {
+            authViewModel.signout()
+        }) { Text("Sign out") }
+        Text(text = "HOME PAGE", fontSize = 30.sp)
+    }
+}
